c154d507537647b3e4e7a65af24d9ddcc9e27f8f
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-13 13:04:46 +0600

    Add vets ,specializations and visits table

--- schema.sql
+++ schema.sql
@@ -25,3 +25,23 @@ CREATE TABLE species (
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD species_id INT REFERENCES species;
 ALTER TABLE animals ADD owner_id INT REFERENCES owners;
+
+CREATE TABLE  vets (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    vets_id INT FOREIGN KEY REFERENCES vets,
+    species_id INT FOREIGN KEY REFERENCES species
+);
+
+CREATE TABLE visits (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    animals_id INT FOREIGN KEY REFERENCES animals,
+    vets_id INT FOREIGN KEY REFERENCES vets,
+    date_of_visit DATE
+);
