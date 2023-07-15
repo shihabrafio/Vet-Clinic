3cf5a3f1046f2dee314b25f1dd25b5ed1b07f12b
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-15 14:10:23 +0600

    Add data

--- data.sql
+++ data.sql
@@ -40,4 +40,4 @@ INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id FROM 
 ((SELECT id FROM animals WHERE name = 'Boarmon'),(SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-02-27'),
 ((SELECT id FROM animals WHERE name = 'Boarmon'),(SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-08-03'),
 ((SELECT id FROM animals WHERE name = 'Blossom'),(SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-05-24'),
-((SELECT id FROM animals WHERE name = 'Blossom'),(SELECT id FROM vets WHERE name = 'William Tatcher'), '2021-01-11'),
+((SELECT id FROM animals WHERE name = 'Blossom'),(SELECT id FROM vets WHERE name = 'William Tatcher'), '2021-01-11');
